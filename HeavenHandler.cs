bff1520cfc372dd085beb3a3bbde6ef1ac010f10
mgnush <mhjorthen@gmail.com>
2019-06-25 22:33:59 +1000

    Add heaven capabilities

---
+++ HeavenHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PCCG_Tester
+{
+    public static class HeavenHandler
+    {
+        public static void InitHeaven(string path)
+        {
+            string filename = Path.Combine(path, "UAC_CONTROL.exe");
+
+            var proc = System.Diagnostics.Process.Start(filename);
+
+        }
+    }
+}
