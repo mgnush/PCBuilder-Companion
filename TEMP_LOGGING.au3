6bb112e825004c0db7c0ec208f4329d8e6bd051c
mgnush <mhjorthen@gmail.com>
2019-06-29 15:12:30 +1000

    Work on temp logging

---
+++ TEMP_LOGGING.au3
@@ -0,0 +1,14 @@
+#cs ----------------------------------------------------------------------------
+
+ AutoIt Version: 3.3.14.5
+ Author:         Magnus Hjorth
+
+ Script Function:
+	Enable CoreTemp logging
+
+#ce ----------------------------------------------------------------------------
+
+WinActivate("Core Temp 1.14")
+Send("{F4}")
+
+WinActivate("PCCG Tester")
--- TempHandler.cs
+++ TempHandler.cs
@@ -16,7 +16,8 @@ namespace PCCG_Tester
     // this might migrate to abstract class
     public static class TempHandler
     {
-        
+        private static int ReadLinesCount = 0;   
+
         public static void InitTemp(string path)
         {
             // CoreTemp for logging purposes (they agree on clock speeds & temps)
@@ -29,7 +30,7 @@ namespace PCCG_Tester
                 Console.WriteLine("File {0} not found.", filename);
             }
 
-            //HWMonitor for details, does not support logging natively
+            // HWMonitor for details, does not support logging natively
             string configPath = Path.Combine(path, "Benchmark/hwmonitorw.ini");
             INIFile hwConfig = new INIFile(configPath);
             hwConfig.Write("UPDATES", "0", "HWMonitor");   // Disable update prompts
@@ -44,18 +45,67 @@ namespace PCCG_Tester
                 Console.WriteLine("File {0} not found.", filename);
             }
 
+            // Launch script to enable logging, see commented section below
+            filename = Path.Combine(path, "TEMP_LOGGING.exe");
+            ProcessStartInfo pInfo = new ProcessStartInfo();
+            pInfo.FileName = filename;
+            pInfo.WorkingDirectory = path;
+            try
+            {
+                Process script = Process.Start(pInfo);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File {0} not found.", filename);
+            }
+            
+            
+            //---Below code enables logging by INI-tampering, but CoreTemp uses a corrupted INI-file. 
+            //---Uncomment code if future update addresses this (or manually declare DTD)
+            //---This approach is favourable to script above
 
-
-            //INIFile tempConfig = new INIFile(@"C://Users/Mgnus/Desktop/CoreTemp64/CoreTemp.ini");
+            //INIFile tempConfig = new INIFile(@"C://Users/User/Desktop/AAA Testing/CoreTemp64/CoreTemp.ini");
             //tempConfig.Write("EnLog", "1", "Core temp settings");
+        }
 
+        public static void ReadTemp(string path)
+        {
+            TempLogWatcher(path);
 
+            using (var fs = new FileStream(fullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var sr = new StreamReader(fs, Encoding.Default))
+            {
+                sr.
+            }
+        }
 
-            //enable logging
+        private static void TempLogWatcher(string path)
+        {
+            string ctFolder = Path.Combine(path, "CoreTemp64");
+            string partialName = "CT-Log";
+            DirectoryInfo folder = new DirectoryInfo(ctFolder);
+            FileInfo[] results = folder.GetFiles(partialName + "*.csv");
+            if (results.Length == 0)
+            {
+                Prompt.ShowDialog("No temp logs were found", "Error");
+                return;
+            }
+            string fullName = results.Last().FullName;   // Choose latest log file
 
-            //tempConfig.DeleteKey("Core Temp settings", "EnLog");
-            //bool moo = tempConfig.KeyExists("EnLog", "Core Temp settings");
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = ctFolder;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = fullName;
+            watcher.Changed += new FileSystemEventHandler(TempUpdate);
+            watcher.EnableRaisingEvents = true;
+        }
 
+        private static void TempUpdate(object source, FileSystemEventArgs e)
+        {
+            int totalLines = File.ReadLines(path).Count();
+            int newLinesCount = totalLines - ReadLinesCount;
+            File.ReadLines(path).Skip(ReadLinesCount).Take(newLinesCount);
+            ReadLinesCount = totalLines;
         }
     }    
      
